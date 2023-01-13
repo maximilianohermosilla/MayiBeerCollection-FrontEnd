1a9e8dd6d4dd5f65b81cac367fb8e1d4757913b4
RYZEN-5600X\maxim <maximiliano_hermosilla@hotmail.com>
2023-01-13 19:46:34 -0300

    Dialogo confirmación Actualizar y Nuevo. Spinner show hide. Fix css dashboard filtros. Fix css grilla cervezas mobile

--- MayiBeerCollectionAngular/src/app/components/ciudad/ciudad.component.ts
+++ MayiBeerCollectionAngular/src/app/components/ciudad/ciudad.component.ts
@@ -1,11 +1,12 @@
 import { Component, Inject, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatTable } from '@angular/material/table';
 import { Ciudad } from 'src/app/models/ciudad';
 import { Pais } from 'src/app/models/pais';
 import { CiudadService } from 'src/app/services/ciudad.service';
 import { SpinnerService } from 'src/app/services/spinner.service';
+import { DialogComponent } from '../shared/dialog/dialog.component';
 
 @Component({
   selector: 'app-ciudad',
@@ -21,7 +22,9 @@ export class CiudadComponent {
   title = "";
   listaPaises: any[] = [];
 
-  constructor(private servicioCiudad: CiudadService, private formBuilder: FormBuilder, public refDialog: MatDialogRef<CiudadComponent>, public spinnerService: SpinnerService,
+  constructor(private servicioCiudad: CiudadService, private formBuilder: FormBuilder,
+     public refDialog: MatDialogRef<CiudadComponent>, public spinnerService: SpinnerService,
+     public dialogoConfirmacion: MatDialog,
     @Inject(MAT_DIALOG_DATA) public data: { ciudad: any, title: string, paises: any[] }) {
     
     this.title = "Nueva ciudad";
@@ -46,13 +49,64 @@ export class CiudadComponent {
     if (this.formGroup.value.pais){
       if (this.datos.id > 0){
         let _edit: Ciudad = {id: this.datos.id, nombre: this.datos.nombre, idPais: this.datos.idPais, nombrePais: this.datos.nombrePais};
-        this.servicioCiudad.actualizar(_edit).subscribe(result =>
-          {this.refDialog.close(this.formGroup.value);}
+        this.servicioCiudad.actualizar(_edit).subscribe(
+          result => 
+          {
+            this.refDialog.close(this.formGroup.value);                   
+            this.dialogoConfirmacion.open(DialogComponent, {
+              width: '400px', data: {
+                titulo: "Confirmación",
+                mensaje: "Ciudad actualizada con éxito",
+                icono: "check_circle",
+                clase: "class-success"
+              }
+            });
+            this.spinnerService.hide();
+          },
+          error => 
+          {
+            this.dialogoConfirmacion.open(DialogComponent, {
+              data: {
+                titulo: "Error",
+                mensaje: error.error,
+                icono: "warning",
+                clase: "class-error"
+              }
+            })
+            this.refDialog.close();
+            console.log(error);
+            this.spinnerService.hide();
+          }          
         );
       }
       else{   
-        this.servicioCiudad.nuevo(this.datos).subscribe(result =>
-          {this.refDialog.close(this.formGroup.value);}
+        this.servicioCiudad.nuevo(this.datos).subscribe(
+          result =>
+          {
+            this.refDialog.close(this.formGroup.value);
+            this.dialogoConfirmacion.open(DialogComponent, {
+              data: {
+                titulo: "Confirmación",
+                mensaje: "Ciudad ingresada con éxito",
+                icono: "check_circle",
+                clase: "class-success"
+              }
+            });
+            this.spinnerService.hide();
+          },
+          error => {
+            this.dialogoConfirmacion.open(DialogComponent, {
+              data: {
+                titulo: "Error",
+                mensaje: error.error,
+                icono: "warning",
+                clase: "class-error"
+              }
+            })
+            this.refDialog.close();
+            console.log(error);
+            this.spinnerService.hide();
+          }
         );
       }
     }
